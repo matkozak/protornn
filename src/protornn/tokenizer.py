057d823b2be8fa53a7f296cb0bdfc6998029987e
Mateusz Kozak <kozak.mateusz@gmail.com>
2025-04-13 18:58:16 +0200

    Add a tokenizer

---
+++ src/protornn/tokenizer.py
@@ -0,0 +1,43 @@
+from typing import Sequence
+
+PAD_TOK = "<pad>"
+UNK_TOK = "<unk>"
+BOS_TOK = "<bos>"
+EOS_TOK = "<eos>"
+
+
+class ProteinTokenizer:
+    def __init__(self):
+        self.special_tokens = (PAD_TOK, UNK_TOK, BOS_TOK, EOS_TOK)
+        self.aa_tokens = "ACDEFGHIKLMNPQRSTVWY"
+        self.vocab = (*self.special_tokens, *self.aa_tokens)
+
+        self.tok_to_idx = {tok: idx for idx, tok in enumerate(self.vocab)}
+        self.idx_to_tok = {idx: tok for idx, tok in enumerate(self.vocab)}
+
+    def encode(
+        self,
+        sequence: str,
+        add_bos: bool = False,
+        add_eos: bool = False,
+        pad_to: int | None = None,
+    ) -> list[int]:
+        tokens = list(sequence)
+
+        if add_bos:
+            tokens.insert(0, BOS_TOK)
+        if add_eos:
+            tokens.append(EOS_TOK)
+
+        encoded = [self.tok_to_idx.get(tok, self.tok_to_idx[UNK_TOK]) for tok in tokens]
+
+        if pad_to is not None:
+            encoded = encoded[:pad_to]  # truncate if pad_to < len(sequence)
+            padding = [self.tok_to_idx[PAD_TOK]] * (pad_to - len(encoded))
+            encoded.extend(padding)
+
+        return encoded
+
+    def decode(self, indices: Sequence[int]) -> str:
+        decoded = "".join(self.idx_to_tok.get(idx, UNK_TOK) for idx in indices)
+        return decoded
