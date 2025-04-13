f7cca2b8e0bf49e5c6738afdab90cf434a4d88d3
Mateusz Kozak <kozak.mateusz@gmail.com>
2025-04-13 17:12:51 +0200

    Add SwissProt dataset
    Add a script to download the SwissProt dataset in FASTA.
    Other datasets or formats TBD.

---
+++ scripts/get_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define data locations
+DATA_DIR="data/external"
+
+# Create directory for downloads
+mkdir -p ${DATA_DIR}
+
+# Download SwissProt
+echo "Downloading SwissProt dataset..."
+wget -P ${DATA_DIR} https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+echo "Extracting SwissProt..."
+gunzip ${DATA_DIR}/uniprot_sprot.fasta.gz
+
+echo "Download complete!"
