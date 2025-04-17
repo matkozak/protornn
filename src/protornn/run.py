cf23b1e7d2f484c96953ed1e61738258e0e3d3b3
Mateusz Kozak <kozak.mateusz@gmail.com>
2025-04-17 19:19:58 +0200

    Add entrypoint script draft

---
+++ src/protornn/run.py
@@ -0,0 +1,23 @@
+import torch
+
+from protornn.data import create_dataloaders
+from protornn.model import ProtoRNN
+from protornn.tokenizer import ProteinTokenizer
+from protornn.train import train_model
+
+
+def main():
+    device = torch.device("cpu")
+    tokenizer = ProteinTokenizer()
+    train_data, val_data, test_data = create_dataloaders(
+        "data/uniprot_sprot.fasta", tokenizer, batch_size=32, sample=0.05
+    )
+    model = ProtoRNN(len(tokenizer.vocab)).to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+    history = train_model(model, train_data, val_data, optimizer, 100, device)
+    for metrics in history:
+        print(metrics)
+
+
+if __name__ == "__main__":
+    main()
